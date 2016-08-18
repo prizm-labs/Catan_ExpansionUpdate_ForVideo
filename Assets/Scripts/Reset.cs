33bd1ac466e573411c5aed218139952d579dc835
taylorjames9 <taylorjames9@gmail.com>
2016-08-18 13:12:21 -0700

    catan now with reset

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			Debug.Log ("We pressed the Q Key");
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+	
+	}
+}
