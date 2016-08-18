a514ca9c8cc296dd7198c2ade9fde711b7dfb6a2
taylorjames9 <taylorjames9@gmail.com>
2016-08-18 10:26:37 -0700

    this is the sequence of the Catan Victory and Updates screens with Michael's graphics

---
+++ Assets/Scripts/Tap4.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TouchScript;
+using TouchScript.Behaviors;
+using TouchScript.Gestures;
+using System;
+
+public class Tap4 : MonoBehaviour {
+
+	public GameObject VictoryScreen;
+	public GameObject UpdateScreen;
+
+
+	void OnEnable(){
+		GetComponent<PressGesture> ().Pressed += Step1_TakeOutVictoryScreen;
+	}
+
+	void OnDisable(){
+		GetComponent<PressGesture> ().Pressed -= Step1_TakeOutVictoryScreen;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Step1_TakeOutVictoryScreen(object sender, EventArgs e) {
+		StartCoroutine ("SwapVictoryForExpansion");
+
+
+	}
+
+
+
+	public IEnumerator SwapVictoryForExpansion(){
+		//Shrink victory screen and red 4
+		VictoryScreen.GetComponent<Animator> ().SetInteger ("Victory_Actions", 1);
+
+		yield return new WaitForSeconds (1.0f); 
+
+		//Pop up Expansion Modal
+		UpdateScreen.GetComponent<Animator> ().SetInteger ("Expansion", 1);
+	}
+
+
+}
---
+++ Assets/Scripts/TapGetUpdates.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TouchScript;
+using TouchScript.Behaviors;
+using TouchScript.Gestures;
+using System;
+
+public class TapGetUpdates : MonoBehaviour {
+
+	public GameObject whiteScrim;
+	public GameObject Green_PlayerDock;
+	public GameObject Red_PlayerDock;
+	public GameObject Yellow_PlayerDock;
+	public GameObject Blue_PlayerDock;
+	public GameObject PirateTracker;
+	public GameObject EventDie;
+
+
+	void OnEnable(){
+		GetComponent<PressGesture> ().Pressed += TakeOutUpdateScreen;
+	}
+
+	void OnDisable(){
+		GetComponent<PressGesture> ().Pressed -= TakeOutUpdateScreen;
+	}
+
+	public void TakeOutUpdateScreen(object sender, EventArgs e) {
+		StartCoroutine ("ExpansionDisappear");
+	}
+
+
+	public IEnumerator ExpansionDisappear(){
+
+		GetComponent<SpriteRenderer> ().enabled = true;
+		yield return new WaitForSeconds (0.5f);
+		GetComponent<SpriteRenderer> ().enabled = false;
+		transform.parent.GetComponent<Animator> ().SetInteger ("Expansion", 2);
+		yield return new WaitForSeconds (2.0f);
+		whiteScrim.GetComponent<SpriteRenderer> ().color = new Color (whiteScrim.GetComponent<SpriteRenderer> ().color.r, whiteScrim.GetComponent<SpriteRenderer> ().color.g, whiteScrim.GetComponent<SpriteRenderer> ().color.b, 0.2f);
+		//yield return new WaitForSeconds (2.0f);
+		Green_PlayerDock.GetComponent<Animator> ().SetInteger ("Bounce", 1);
+		yield return new WaitForSeconds (0.1f);
+		Red_PlayerDock.GetComponent<Animator> ().SetInteger ("Bounce", 1);
+		yield return new WaitForSeconds (0.2f);
+		Yellow_PlayerDock.GetComponent<Animator> ().SetInteger ("Bounce", 1);
+		yield return new WaitForSeconds (0.1f);
+		Blue_PlayerDock.GetComponent<Animator> ().SetInteger ("Bounce", 1);
+		yield return new WaitForSeconds (1.0f);
+		PirateTracker.GetComponent<Animator> ().SetInteger ("Bounce", 1);
+		EventDie.GetComponent<Animator> ().SetInteger ("Bounce", 1);
+
+
+	}
+
+}
